d16172d2e92258b0b10d3d2fb809a55358cdfd28
mamedelp <mamedelp@ncsu.edu>
2022-10-22 18:58:51 -0400

    [SKELETON] start implementing cleaning JSON

--- backend/server.py
+++ backend/server.py
@@ -29,27 +29,4 @@ def upload_file():
             #     filename.rsplit('.', 1)[1].lower()
             file.save(os.path.join(filename))
         return Response(parse(filename))     
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-        # if 'file' not in request.files:
-        #     flash('No file part')
-        #     return redirect('/')
-#         file = request.files['file']
-#         # If the user does not select a file, the browser submits an
-#         # empty file without a filename.
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect("/")
-#         if file and allowed_file(file.filename):
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-#             return redirect(url_for('download_file', name=file.filename))
-#     return '''
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form method=post enctype=multipart/form-data>
-#       <input type=file name=file>
-#       <input type=submit value=Upload>
-#     </form>
-#     '''
+
