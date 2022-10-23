beb10d4800caabaeb036b68a19acbccddfbdbe05
mamedelp <mamedelp@ncsu.edu>
2022-10-22 22:35:27 -0400

    [IMPLEMENTATION] added functionality to clean JSON output for relevant information

--- backend/cleanOutput.py
+++ backend/cleanOutput.py
@@ -1,64 +1,60 @@
-from dataclasses import dataclass
-import dacite
 import json
-# @dataclass
-# class Body: 
-#     day:int
-#     month:int
-#     year:int
 
-# @dataclass
-# class Dat:
-#     response_time: int
-#     body: Body
-# data = {'response_time':12, 'body':{'day':1,'month':2,'year':3}}
 
-# dat: Dat = dacite.from_dict(Dat,data)
-# print(dat)
-myDict = {}
-  
-# Opening JSON file
-f = open('output.json')
-  
-# returns JSON object as 
-# a dictionary
-data = json.load(f)
-  
-# Iterating through the json
-# list
-# for i in data['data']:
-data = data["data"]
-
-myDict['first_name'] = data["name"]["first"]
-myDict['last_name'] = data["name"]["last"]
-myDict['phone_number'] = data["phone_numbers"][0]
-myDict['website'] = data["websites"][0]
-myDict[ 'emails'] = data['emails'][0]
-myDict['state'] = data["location"]["state"]
-myDict['country'] = data["location"]["country"]
-myDict["city"] = data["location"]["city"]
-
-i = 1
-for edu in data["education"]:
-    myDict[f"university_{i}"] = edu["organization"]
-    myDict[f"education_{i}"] = edu["accreditation"]["input_str"]
-    myDict[f"gpa_{i}"] = edu["raw"]
-    myDict[f"location_{i}"] = ["location"]["raw_input"]
-    i += 1
-    # myDict[]
-
-i = 1
-for work in data["work_experience"]:
-    myDict[f"job_{i}"] = work["job_title"]
-    myDict[f"location_{i}"] = work["location"]["raw_input"]
-    myDict[f"description_{i}"] = work["job_description"]
-    myDict[f"endDate_{i}"] = work["dates"]["end_date"]
-    i += 1
-
-i = 1
-for skill in data["skills"]:
-                               
+def cleanJSON():
+    myDict = {}
     
-  
-# Closing file
-f.close()
+    # Opening JSON file
+    f = open('output.json')
+    
+    # returns JSON object as 
+    # a dictionary
+    data = json.load(f)
+    
+    # Iterating through the json
+    # list
+    # for i in data['data']:
+    data = data["data"]
+
+    myDict['first_name'] = data["name"]["first"]
+    myDict['last_name'] = data["name"]["last"]
+    myDict['phone_number'] = data["phone_numbers"][0]
+    # myDict['website'] = data["websites"][0]
+    myDict[ 'emails'] = data['emails'][0]
+    myDict['state'] = data["location"]["state"]
+    myDict['country'] = data["location"]["country"]
+    myDict["city"] = data["location"]["city"]
+
+    i = 1
+    for edu in data["education"]:
+        myDict[f"university_{i}"] = edu["organization"]
+        myDict[f"education_{i}"] = edu["accreditation"]["input_str"]
+        myDict[f"gpa_{i}"] = edu["grade"]["raw"]
+        myDict[f"location_{i}"] = edu["location"]["raw_input"]
+        i += 1
+    myDict[f"education_count"] = i 
+        
+
+    i = 1
+    for work in data["work_experience"]:
+        myDict[f"job_{i}"] = work["job_title"]
+        myDict[f"location_{i}"] = work["location"]["raw_input"]
+        myDict[f"description_{i}"] = work["job_description"]
+        if "dates" in work:
+            myDict[f"endDate_{i}"] = work["dates"]["end_date"]
+        i += 1
+    myDict[f"work_count"] = i 
+
+
+    i = 1
+    for skill in data["skills"]:
+        myDict[f"skill_{i}"] = skill['name']                                   
+        
+    for section in data["sections"]:
+        if section["section_type"] == "Projects":
+            myDict["skills"] =  section["text"]
+            break
+    myDict["approval_status"] = "neutral" 
+    print(myDict)
+    
+cleanJSON()
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,12 @@
 import os
+import logging
 from pdf2json import parse
 from flask import Flask, Response, flash, redirect, request, url_for
+from argparse import ArgumentParser, RawTextHelpFormatter
+import psycopg
+from psycopg.errors import SerializationFailure, Error
+from psycopg.rows import namedtuple_row
+
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = {'pdf', 'docx'}
 
@@ -25,8 +31,54 @@ def upload_file():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = file.filename
-            # filename = "A" + str(uuid.uuid1()) + "." + \
-            #     filename.rsplit('.', 1)[1].lower()
             file.save(os.path.join(filename))
-        return Response(parse(filename))     
+        return Response(parse(filename))
+    
+@app.route('/applicant_data', methods=['GET'])
+def getApplicantData():
+    opt = parse_cmdline()
+    logging.basicConfig(level=logging.DEBUG if opt.verbose else logging.INFO)
+    try:
+        # Attempt to connect to cluster with connection string provided to
+        # script. By default, this script uses the value saved to the
+        # DATABASE_URL environment variable.
+        # For information on supported connection string formats, see
+        # https://www.cockroachlabs.com/docs/stable/connect-to-the-database.html.
+        db_url = opt.dsn
+        conn = psycopg.connect(db_url, 
+                               application_name="$ docs_simplecrud_psycopg3", 
+                               row_factory=namedtuple_row)
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM public.applicant1")
 
+    except Exception as e:
+        logging.fatal("database connection failed")
+        logging.fatal(e)
+        return
+    #select * from APPLICANTS
+    # for every id:
+    # get all 5 tables 
+def parse_cmdline():
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument("-v", "--verbose",
+                        action="store_true", help="print debug info")
+
+    parser.add_argument(
+        "dsn",
+        default=os.environ.get("DATABASE_URL"),
+        nargs="?",
+        help="""\
+database connection string\
+ (default: value of the DATABASE_URL environment variable)
+            """,
+    )
+
+    opt = parser.parse_args()
+    if opt.dsn is None:
+        parser.error("database connection string not set")
+    return opt      
+
+# if __name__ == "__main__":
+#     main()
